2c29d7cc72c3ad9a85c557af75905338b7f8c77b
debiantarte <debiantarte@minet.net>
2019-11-23 14:11:39 +0100

    small changes to slimes added earth and tree to a scene the test factory scene now has slimes generating and cumulating on other side of planet

--- Assets/Scripts/SlimeController.cs
+++ Assets/Scripts/SlimeController.cs
@@ -8,9 +8,12 @@ public class SlimeController : MonoBehaviour
 
     [SerializeField] private float speed = 1;
     [SerializeField] private Vector3 goal = Vector3.zero;
+    public Vector3 Goal { get => goal; set => goal = value; }
+
     [SerializeField] private float gravity = 0.5f;
     [SerializeField] private float goalThreshold = 1;
     private Vector3 currentSpeed =  Vector3.zero;
+
 
     // Update is called once per frame
     void Update()
@@ -22,7 +25,7 @@ public class SlimeController : MonoBehaviour
     public bool IsGoalReached()
     {
         // TODO : if the cube is less than goalThreshold from the goal, we teleport it and return true
-        if ((transform.position - goal).magnitude < goalThreshold)
+        if ((transform.position - Goal).magnitude < goalThreshold)
         {
             // TODO : move the color change to the Avatar
             GetComponent<SpriteRenderer>().color = Color.grey;
@@ -38,7 +41,7 @@ public class SlimeController : MonoBehaviour
         // TODO : slimey movement
         // TODO : orientation of the slime (always with the gravity down)
         var position = transform.position;
-        Vector3 toGoal = (goal - position).normalized;
+        Vector3 toGoal = (Goal - position).normalized;
         currentSpeed = speed * toGoal + gravity * (Vector3.zero - position).normalized;
         position = position + Time.deltaTime * currentSpeed;
         transform.position = position;
