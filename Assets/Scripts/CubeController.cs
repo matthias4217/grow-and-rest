c0e3e261a046570c0f50fb92132ffdbe71dad463
ombre⛧ <matthias.goffette@telecom-sudparis.eu>
2019-11-23 12:08:00 +0100

    Adds basic movement

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class CubeController : MonoBehaviour
 {
+
+    [SerializeField] private float speed;
+    private Vector3 goal;
+    private Vector3 currentSpeed;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        goal = Vector3.zero;
+        currentSpeed = Vector3.right;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveTowardsGoal();
+    }
+
+    private void MoveTowardsGoal()
+    {
+        // TODO : slimey movement
+        var position = transform.position;
+        Vector2 toGoal = goal - position;
+        currentSpeed = speed * toGoal;
+        position = position + Time.deltaTime * currentSpeed;
+        transform.position = position;
     }
 }
