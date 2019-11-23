01c331161affb97420d35e984a654b9d02dd6b8e
ombre⛧ <matthias.goffette@telecom-sudparis.eu>
2019-11-23 12:59:40 +0100

    [Cube] Adds function to detect if goal reached

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -9,18 +9,27 @@ public class CubeController : MonoBehaviour
     [SerializeField] private float speed = 1;
     [SerializeField] private Vector3 goal = Vector3.zero;
     [SerializeField] private float gravity = 1;
+    [SerializeField] private float goalThreshold = 1;
     private Vector3 currentSpeed =  Vector3.zero;
 
     // Update is called once per frame
     void Update()
     {
         MoveTowardsGoal();
+        IsGoalReached();
     }
 
-    private bool isGoalReached()
+    public bool IsGoalReached()
     {
-        // TODO : if the cube is less than epsilon from the goal, we teleport it and return true
-        throw new NotImplementedException();
+        // TODO : if the cube is less than goalThreshold from the goal, we teleport it and return true
+        if ((transform.position - goal).magnitude < goalThreshold)
+        {
+            GetComponent<SpriteRenderer>().color = Color.grey;
+            return true;
+        }
+
+        return false;
+        //throw new NotImplementedException();
     }
 
     private void MoveTowardsGoal()
