467eea0763108fddb1afb1ca553e91e0c3343029
debiantarte <debiantarte@minet.net>
2019-11-23 12:22:28 +0100

    Simple factory for slime generation with generator for testing

---
+++ Assets/Scripts/SlimeAvatar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SlimeState
+{
+    living,
+    dead
+}
+
+public class SlimeAvatar : MonoBehaviour
+{
+    [SerializeField] private SlimeState state;
+    public SlimeState State
+    {
+        get { return state; }
+        set { state = value; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        SlimeFactory.Release(this);
+    }
+}
---
+++ Assets/Scripts/SlimeFactory.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeFactory : MonoBehaviour
+{
+    private static SlimeFactory instance;
+
+    [SerializeField] private GameObject slimePrefab;
+    [SerializeField] private int slimesToPreinstantiate;
+
+    private static Queue<SlimeAvatar> availableSlimes = new Queue<SlimeAvatar>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static SlimeAvatar GetSlime(Vector2 SpawnPosition)
+    {
+        SlimeAvatar slime = null;
+        if (availableSlimes.Count > 0)
+        {
+            slime = availableSlimes.Dequeue();
+        }
+        if (slime == null)
+        {
+            slime = CreateSlime();
+        }
+
+        slime.gameObject.SetActive(true);
+        slime.transform.position = SpawnPosition;
+        return slime;
+    }
+
+    public static SlimeAvatar CreateSlime()
+    {
+        GameObject slimeInstance = Instantiate(instance.slimePrefab);
+        slimeInstance.transform.parent = instance.transform;
+        slimeInstance.SetActive(false);
+        SlimeAvatar slime = slimeInstance.GetComponent<SlimeAvatar>();
+        return slime;
+    }
+
+    public static void Release(SlimeAvatar slime)
+    {
+        slime.gameObject.SetActive(false);
+        availableSlimes.Enqueue(slime);
+    }
+}
