af51905b48d90400e93204a88633ce6fdc1bb0b3
ombre⛧ <matthias.goffette@telecom-sudparis.eu>
2019-11-23 12:32:42 +0100

    Adds gravity 🌎

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -1,20 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CubeController : MonoBehaviour
 {
 
-    [SerializeField] private float speed;
-    private Vector3 goal;
-    private Vector3 currentSpeed;
-
-    // Start is called before the first frame update
-    void Awake()
-    {
-        goal = Vector3.zero;
-        currentSpeed = Vector3.right;
-    }
+    [SerializeField] private float speed = 1;
+    [SerializeField] private Vector3 goal = Vector3.zero;
+    [SerializeField] private float gravity = 1;
+    private Vector3 currentSpeed =  Vector3.zero;
 
     // Update is called once per frame
     void Update()
@@ -22,12 +17,18 @@ public class CubeController : MonoBehaviour
         MoveTowardsGoal();
     }
 
+    private bool isGoalReached()
+    {
+        // TODO : if the cube is less than epsilon from the goal, we teleport it and return true
+        throw new NotImplementedException();
+    }
+
     private void MoveTowardsGoal()
     {
         // TODO : slimey movement
         var position = transform.position;
-        Vector2 toGoal = goal - position;
-        currentSpeed = speed * toGoal;
+        Vector3 toGoal = (goal - position).normalized;
+        currentSpeed = speed * toGoal + gravity * (Vector3.zero - position).normalized;
         position = position + Time.deltaTime * currentSpeed;
         transform.position = position;
     }
