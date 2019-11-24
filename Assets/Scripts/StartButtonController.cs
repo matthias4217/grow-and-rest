9d37a89008ca86adfcab30b068cc4e264069400a
debiantarte <debiantarte@minet.net>
2019-11-24 14:42:12 +0100

    retweaked ui

--- Assets/Scripts/StartButtonController.cs
+++ Assets/Scripts/StartButtonController.cs
@@ -25,6 +25,6 @@ public class StartButtonController : MonoBehaviour
     public void MyJobIsDone()
     {
         //ui.gameObject.SetActive(false);
-        gameObject.SetActive(false);
+        ui.gameObject.SetActive(false);
     }
 }
